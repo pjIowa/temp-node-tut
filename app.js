07b5060f11f6f36fa2cb6269547bda1696f12fd9
Prajwal Kedilaya <pjkedilaya@gmail.com>
2022-11-27 00:02:17 -0800

    add script for async await

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-//node commands
-//npm --version
-//npm i PACKAGE_NAME, installs package in local project only
-//npm install -g PACKAGE_NAME, installs package globally
-//package.json, manifest file of what packages used in local project
-    //can be created manually at root folder or with below commands
-//npm init, asks to select among options
-//npm init -y, uses all defaults
+const {readFile, writeFile} = require('fs').promises;
 
-const _ = require('lodash');
+const start = async() => {
+    try{
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/test.txt', 'utf8');
+        await writeFile('./content/result-async.txt',
+            `Result: ${first}, ${second}`
+        );
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+        console.log(first);
+        console.log(second);
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+start();
+
+
+
+
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+// const getText = (path) =>{
+//     return new Promise((resolve, reject)=>{
+//         readFile(path, 'utf8', (err, data) =>{
+//             if(err){
+//                 reject(err);
+//             }
+//             else{
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
+
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err));
